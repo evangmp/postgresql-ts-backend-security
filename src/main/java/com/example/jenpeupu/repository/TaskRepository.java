f66c079d3cc9309316a969e407c7368ec35aee08
Evan <evangicq@gmail.com>
2024-07-03 13:09:04 +0200

    security tutorial update, all is good

--- src/main/java/com/example/jenpeupu/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jenpeupu.repository;
-
-import com.example.jenpeupu.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-}
---
+++ src/main/java/security/models/ERole.java
@@ -0,0 +1,7 @@
+package security.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
