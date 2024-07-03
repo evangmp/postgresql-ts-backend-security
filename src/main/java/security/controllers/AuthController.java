e6d9b5c51f30ff4591230d557b745d787284d81c
Evan <evangicq@gmail.com>
2024-07-03 23:38:41 +0200

    none modification

--- src/main/java/security/controllers/AuthController.java
+++ src/main/java/security/controllers/AuthController.java
@@ -47,7 +47,6 @@ public class AuthController {
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-
         Authentication authentication = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
